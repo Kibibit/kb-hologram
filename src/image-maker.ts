7b069990000ee6cac51d389ec53e20b94482bb7a
thatkookooguy <neilkalman@gmail.com>
2023-10-20 09:53:26 +0300

    fix(deps): update module to latest deps
    also fix tests

--- src/image-maker.ts
+++ src/image-maker.ts
@@ -1,27 +1,27 @@
-import fileType from 'file-type';
-import { readdir, readFile } from 'fs-extra';
-import { compile } from 'handlebars';
-import { isString, startsWith } from 'lodash';
-import { join } from 'path';
-import puppeteer from 'puppeteer';
-import svg2png from 'svg2png';
+import fileType from "file-type";
+import { readdir, readFile, readFileSync } from "fs-extra";
+import { compile } from "handlebars";
+import { isString, startsWith } from "lodash";
+import { join } from "path";
+import puppeteer from "puppeteer";
+import svg2png from "svg2png";
 
 export enum SvgMakerResultType {
-  Base64Png = 'Base64Png',
-  Base64Svg = 'Base64Svg',
-  SvgString = 'SvgString',
-  SvgBuffer = 'SvgBuffer',
-  PngBuffer = 'PngBuffer'
+  Base64Png = "Base64Png",
+  Base64Svg = "Base64Svg",
+  SvgString = "SvgString",
+  SvgBuffer = "SvgBuffer",
+  PngBuffer = "PngBuffer",
 }
 
 export type StringReturnInputs =
-  SvgMakerResultType.Base64Png |
-  SvgMakerResultType.Base64Svg |
-  SvgMakerResultType.SvgString;
+  | SvgMakerResultType.Base64Png
+  | SvgMakerResultType.Base64Svg
+  | SvgMakerResultType.SvgString;
 
 export type BufferReturnInputs =
-  SvgMakerResultType.SvgBuffer |
-  SvgMakerResultType.PngBuffer;
+  | SvgMakerResultType.SvgBuffer
+  | SvgMakerResultType.PngBuffer;
 
 export interface ISvgMakerBaseOptions {
   templateName?: string;
@@ -31,50 +31,56 @@ export interface ISvgMakerBaseOptions {
   width: number;
   height: number;
   data: {
-    [ key: string ]: any;
+    [key: string]: any;
   };
-  type?: 'svg' | 'html';
+  type?: "svg" | "html";
 }
 
 export class SvgMaker {
-  private templateFilePath = '';
+  private templateFilePath = "";
   // constructor(options: ISvgMakerNameOptions)
   // constructor(options: ISvgMakerFileOptions)
   // constructor(options: ISvgMakerStringOptions)
-  constructor(public options: ISvgMakerBaseOptions) { }
+  constructor(public options: ISvgMakerBaseOptions) {}
 
   async render(resultType: StringReturnInputs): Promise<string>;
   async render(resultType: BufferReturnInputs): Promise<Buffer>;
-  async render(resultType: SvgMakerResultType = SvgMakerResultType.SvgString): Promise<string | Buffer> {
+  async render(
+    resultType: SvgMakerResultType = SvgMakerResultType.SvgString
+  ): Promise<string | Buffer> {
     try {
       const template = await this.getTemplateAsString();
 
       const handlebarsTemplate = compile(template);
-      const fontBase64 = this.options.fontName ?
-        await this.convertFontToBase64String(join(__dirname, this.options.fontName)) :
-        undefined;
+      const fontBase64 = this.options.fontName
+        ? await this.convertFontToBase64String(
+            join(__dirname, this.options.fontName)
+          )
+        : undefined;
 
       const data = {
         ...this.options.data,
         fontBase64,
         width: this.options.width,
-        height: this.options.height
+        height: this.options.height,
       };
 
       const svgString = handlebarsTemplate(data);
 
-      if (this.options.type === 'html') {
-        const browser = await puppeteer.launch();
+      if (this.options.type === "html") {
+        const browser = await puppeteer.launch({
+          headless: "new",
+        });
         const page = await browser.newPage();
         await page.setViewport({
           height: this.options.height,
-          width: this.options.width
+          width: this.options.width,
         });
-        await page.goto('file://' + this.templateFilePath);
+        await page.goto("file://" + this.templateFilePath);
         await page.addScriptTag({
           content: `
-          activate(${ JSON.stringify(this.options.data) })
-          `
+          activate(${JSON.stringify(this.options.data)})
+          `,
         });
         const imageBuffer = await page.screenshot({});
 
@@ -91,7 +97,7 @@ export class SvgMaker {
         return svgString;
       }
 
-      const svgFile = Buffer.from(svgString, 'utf8');
+      const svgFile = Buffer.from(svgString, "utf8");
 
       if (resultType === SvgMakerResultType.SvgBuffer) {
         return svgFile;
@@ -99,14 +105,14 @@ export class SvgMaker {
 
       if (resultType === SvgMakerResultType.Base64Svg) {
         return this.bufferToBase64String(svgFile, {
-          mime: 'image/svg+xml',
-          ext: 'svg'
+          mime: "image/svg+xml",
+          ext: "svg",
         });
       }
 
       const pngFile = await svg2png(svgFile, {
         height: this.options.height,
-        width: this.options.width
+        width: this.options.width,
       });
 
       if (resultType === SvgMakerResultType.PngBuffer) {
@@ -126,48 +132,52 @@ export class SvgMaker {
     const buff = Buffer.from(data);
     const filetype = this.fileType(buff);
 
-    const base64data = buff.toString('base64');
+    const base64data = buff.toString("base64");
 
     // writeFileSync('font-base64.txt', `data:${ filetype.mime };base64,${ base64data }`);
 
-    return `data:${ filetype.mime };base64,${ base64data }`;
+    return `data:${filetype.mime};base64,${base64data}`;
   }
 
-  bufferToBase64String(buff: Buffer, filetype: any = this.fileType(buff)): string {
-    return `data:${ filetype.mime };base64,${ buff.toString('base64') }`;
+  bufferToBase64String(
+    buff: Buffer,
+    filetype: any = this.fileType(buff)
+  ): string {
+    return `data:${filetype.mime};base64,${buff.toString("base64")}`;
   }
 
   fileType(buff: Buffer) {
     const filetype = fileType(buff);
 
     if (!filetype) {
-      throw new Error('filetype returned undefined');
+      throw new Error("filetype returned undefined");
     }
 
     return filetype;
   }
 
   async getTemplateAsString(): Promise<string> {
-    let template = '';
+    let template = "";
 
     if (isString(this.options.templateName)) {
-      const allTemplates = await readdir(join(__dirname, '../templates'));
+      const allTemplates = await readdir(join(__dirname, "../templates"));
 
-      const templateFilename = allTemplates
-        .find((templateFilename) => startsWith(templateFilename, this.options.templateName));
+      const templateFilename = allTemplates.find((templateFilename) =>
+        startsWith(templateFilename, this.options.templateName)
+      );
 
       if (!templateFilename) {
-        throw new Error('template filename not found');
+        throw new Error("template filename not found");
       }
 
-      this.templateFilePath = join(__dirname, '../templates', templateFilename);
+      this.templateFilePath = join(__dirname, "../templates", templateFilename);
 
-      const filePath = join(__dirname, '../templates', `/${ templateFilename }`);
-      template = await readFile(filePath, { encoding: 'UTF-8' });
+      const filePath = join(__dirname, "../templates", `/${templateFilename}`);
+      template = await readFile(filePath, { encoding: "UTF-8" });
     } else if (this.options.templateFile) {
-      template = isString(this.options.templateFile) ?
-        await readFile(this.options.templateFile, { encoding: 'UTF-8' }) :
-        this.options.templateFile.toString();
+      template = isString(this.options.templateFile)
+        ? await readFile(this.options.templateFile, { encoding: "UTF-8" })
+        : this.options.templateFile.toString();
     } else if (this.options.templateString) {
       template = this.options.templateString;
     }
