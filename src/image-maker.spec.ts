98872e87fbd61349b694a319358ecc9ca22f953f
Neil Kalman <neil.kalman@growthspace.com>
2024-11-28 01:42:17 +0200

    chore(tests): make timeout bigger

--- src/image-maker.spec.ts
+++ src/image-maker.spec.ts
@@ -131,7 +131,7 @@ describe("image maker", () => {
     // await writeFile('nice.svg', pngBuffer);
 
     (expect(pngBuffer) as any).toMatchImageSnapshot(customConfig);
-  }, 15000);
+  }, 20000);
 
   it("should generate image from html template with external template", async () => {
     const kbHologram = new KbHologram({
